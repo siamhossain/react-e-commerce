5c7c9623338ab377effd4d545e4df24e4d07d2c2
Siam Hossain <siam.hossain1994@gmail.com>
2020-11-17 01:02:33 +0600

    Cart component Header

--- src/context.js
+++ src/context.js
@@ -10,6 +10,9 @@ class ProductProvider extends Component {
         cart: [],
         modalOpen:false,
         modalProduct:detailProduct,
+        cartSubTotal:0,
+        cartTax:0,
+        cartTotal:0
     };
     componentDidMount() {
         this.setProducts();
@@ -66,6 +69,22 @@ class ProductProvider extends Component {
         })
     }
 
+    increment = (id) =>{
+        console.log('this is increment method')
+    }
+
+    decrement = (id) =>{
+        console.log('this is decrement method')
+    }
+
+    removeItem = (id) =>{
+        console.log('item removed')
+    }
+
+    clearCart = () =>{
+        console.log('cart was cleared')
+    }
+
     render() {
         return (
             <ProductContext.Provider 
@@ -74,7 +93,12 @@ class ProductProvider extends Component {
                     handleDetail: this.handleDetail,
                     addToCart: this.addToCart,
                     openModal:this.openModal,
-                    closeModal:this.closeModal
+                    closeModal:this.closeModal,
+                    increment: this.increment,
+                    decrement:this.decrement,
+                    removeItem:this.removeItem,
+                    clearCart:this.clearCart
+
                 }}> 
                 {this.props.children}
             </ProductContext.Provider> 
