cd3c5f2345474cfba775fcff9479005b5cd554a9
Siam Hossain <siam.hossain1994@gmail.com>
2020-11-19 03:59:54 +0600

    Add product quantity increment and decrement

--- src/context.js
+++ src/context.js
@@ -69,13 +69,50 @@ class ProductProvider extends Component {
         })
     }
 
-    increment = (id) =>{
-        console.log('this is increment method')
-    }
+    increment = id =>{
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id ===id)
 
-    decrement = (id) =>{
-        console.log('this is decrement method')
-    }
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count +1;
+        product.total = product.count * product.price;
+
+        this.setState(()=>{
+            return {
+                cart:[...tempCart]
+            }
+        },()=>{
+            this.addTotals();
+        })
+    };
+
+    decrement = id =>{
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id ===id)
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count - 1;
+
+        if(product.count === 0){
+            this.removeItem(id)
+        }
+        else{
+            product.total = product.count * product.price;
+            
+            this.setState(()=>{
+                return {
+                    cart:[...tempCart]
+                }
+            },()=>{
+                this.addTotals();
+            })
+        }
+
+    };
 
     removeItem = (id) =>{
         let tempProducts = [...this.state.products];
