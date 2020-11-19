79b15e606ff4f9d157b890ea0b323bbb357cef63
Siam Hossain <siam.hossain1994@gmail.com>
2020-11-20 00:20:33 +0600

    Updates

---
+++ precache-manifest.13d4cfb0d87bd930aa6c6b16d23fc9ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac6e910c8afa7de7ba71bb5d765a517",
+    "url": "/react-e-commerce/index.html"
+  },
+  {
+    "revision": "45a2dab84ba0c3d84aa1",
+    "url": "/react-e-commerce/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8a968fe8d972cbca7b74",
+    "url": "/react-e-commerce/static/css/main.6a487ca2.chunk.css"
+  },
+  {
+    "revision": "45a2dab84ba0c3d84aa1",
+    "url": "/react-e-commerce/static/js/2.223a1d10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-e-commerce/static/js/2.223a1d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a968fe8d972cbca7b74",
+    "url": "/react-e-commerce/static/js/main.f50d6d01.chunk.js"
+  },
+  {
+    "revision": "0e3b18df545e6a661851",
+    "url": "/react-e-commerce/static/js/runtime-main.8d716ff8.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-e-commerce/static/media/logo.9f7e506d.svg"
+  }
+]);
